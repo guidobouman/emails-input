42ebca3b152e2127fd299db57467d81a16b97ce1
Guido <guido@q42.nl>
2020-12-05 14:38:11 +0100

    set main file

--- src/emails-input.ts
+++ src/emails-input.ts
@@ -6,6 +6,10 @@ class EmailsInput {
       throw new Error('Did you forget to provide a container?');
     }
 
+    if(container.hasChildNodes()) {
+      console.warn('The container does not seem to be empty, are you sure?', container)
+    }
+
     this.container = container;
   }
 }
