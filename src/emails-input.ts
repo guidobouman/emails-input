19617eddf6b19e0cd1ff8db9d7043a5c726ee449
Guido <guido@q42.nl>
2020-12-05 17:55:02 +0100

    setup eventListener

--- src/emails-input.ts
+++ src/emails-input.ts
@@ -3,6 +3,8 @@ class EmailsInput {
   listContainer: HTMLSpanElement;
   inputElement: HTMLInputElement;
 
+  delimiter: string = ',';
+
   constructor(container: HTMLElement) {
     if(!container) {
       throw new Error('Did you forget to provide a container?');
@@ -19,6 +21,7 @@ class EmailsInput {
 
   setup() {
     this.scaffoldStructure();
+    this.bindEventListeners();
   }
 
   scaffoldStructure() {
@@ -26,5 +29,39 @@ class EmailsInput {
     this.inputElement = document.createElement('input');
 
     this.container.append(this.listContainer, this.inputElement);
+  }
+
+  bindEventListeners() {
+    // this.inputElement.addEventListener('keydown', this.processInput.bind(this));
+    this.inputElement.addEventListener('input', this.processInput.bind(this));
+    this.inputElement.addEventListener('keydown', this.processKeydown.bind(this));
+    this.inputElement.addEventListener('blur', this.processBlur.bind(this));
+  }
+
+  processInput(event: InputEvent) {
+    const input = this.inputElement.value;
+    if(input.indexOf(this.delimiter) == -1) {
+      return;
+    }
+
+    const entries = input.split(this.delimiter);
+
+    console.log(entries);
+
+    entries.forEach(this.addEntry);
+  }
+
+  processKeydown(event: KeyboardEvent) {
+    if(event.key === 'enter') {
+      this.addEntry();
+    }
+  }
+
+  processBlur(event: FocusEvent) {
+    this.addEntry();
+  }
+
+  addEntry(entry: string = this.inputElement.value) {
+    console.log(entry);
   }
 }
