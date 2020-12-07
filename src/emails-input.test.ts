defa2927d04c1f702e007354b8ce739e68ebcd4a
Guido <guido@q42.nl>
2020-12-07 12:37:17 +0100

    add addEntry tests

--- src/emails-input.test.ts
+++ src/emails-input.test.ts
@@ -29,3 +29,46 @@ describe('class constructor', () => {
     spy.mockRestore();
   });
 });
+
+describe('addEntry', () => {
+  test('should be able to add entry', () => {
+    const container = document.createElement('div');
+    const instance = new EmailsInput(container);
+
+    const entry = instance.addEntry('test@example.com');
+    expect(entry.string).toBe('test@example.com');
+  });
+
+  test('should mark valid entries', () => {
+    const container = document.createElement('div');
+    const instance = new EmailsInput(container);
+
+    const entry = instance.addEntry('test@example.com');
+    expect(entry.isValid).toBe(true);
+
+    const entryTwo = instance.addEntry('this-is@valid');
+    expect(entryTwo.isValid).toBe(true);
+  });
+
+  test('should mark invalid entries', () => {
+    const container = document.createElement('div');
+    const instance = new EmailsInput(container);
+
+    const entry = instance.addEntry('invalid.email');
+    expect(entry.isValid).toBe(false);
+
+    const entryTwo = instance.addEntry('@invalid.email');
+    expect(entryTwo.isValid).toBe(false);
+
+    const entryThree = instance.addEntry('this-is@invalid.');
+    expect(entryThree.isValid).toBe(false);
+  });
+
+  test('should strip whitespace', () => {
+    const container = document.createElement('div');
+    const instance = new EmailsInput(container);
+
+    const entry = instance.addEntry('  test@example.com  ');
+    expect(entry.string).toBe('test@example.com');
+  });
+});
