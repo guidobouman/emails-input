861441427fa72d595a87a5e31f7ec9e9e290cec8
Guido <guido@q42.nl>
2020-12-07 12:36:37 +0100

    join class tests in single describe

--- src/emails-input.test.ts
+++ src/emails-input.test.ts
@@ -2,29 +2,30 @@ import { expect } from '@jest/globals';
 
 import { EmailsInput } from '../dist/emails-input';
 
-test('should be able to instantiate', () => {
-  const container = document.createElement('div');
-  const instance = new EmailsInput(container);
+describe('class constructor', () => {
+  test('should be able to instantiate', () => {
+    const container = document.createElement('div');
+    const instance = new EmailsInput(container);
 
-  expect(instance).toBeInstanceOf(EmailsInput);
-});
+    expect(instance).toBeInstanceOf(EmailsInput);
+  });
 
-test('should break on missing container', () => {
-  expect(() => {
-    new EmailsInput();
-  }).toThrowError("Did you forget to provide a container?");
-});
+  test('should break on missing container', () => {
+    expect(() => {
+      new EmailsInput();
+    }).toThrowError("Did you forget to provide a container?");
+  });
 
+  test('should warn on non-empty container', () => {
+    const spy = jest.spyOn(global.console, 'warn').mockImplementation(() => {});
 
-test('should warn on non-empty container', () => {
-  const spy = jest.spyOn(global.console, 'warn').mockImplementation(() => {});
+    const container = document.createElement('div');
+    container.textContent = "not empty";
 
-  const container = document.createElement('div');
-  container.textContent = "not empty";
+    expect(spy).not.toHaveBeenCalled();
+    new EmailsInput(container);
+    expect(spy).toHaveBeenCalled();
 
-  expect(spy).not.toHaveBeenCalled();
-  new EmailsInput(container);
-  expect(spy).toHaveBeenCalled();
-
-  spy.mockRestore();
+    spy.mockRestore();
+  });
 });
